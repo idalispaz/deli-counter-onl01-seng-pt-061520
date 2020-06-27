55b63d373bd728ecf72a1a9f804ececd09154b6b
Idalis Paz <idalispaz@gmail.com>
2020-06-27 02:13:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,15 +2,3 @@ katz_deli = []
 other_deli = ["logan", "Avi","Spencer"]
 another_deli = ["Amanda","Annette","Ruchi","Jason","Logan","Spencer", "Avi","Joe","Rachel","Lindsey"]
 
-def line(katz_deli)
-  queue_array = []
-  if katz_deli.length == 0 
-    puts "The line is currently empty."
-  end 
-end 
-
-  context “there are people in line” do
-      it “should display the current line” do
-        expect($stdout).to receive(:puts).with(“The line is currently: 1. Logan 2. Avi 3. Spencer”)
-        line(other_deli)
-      end
