8a1c3f036553785df0be43226ff69890bafa2b76
Idalis Paz <idalispaz@gmail.com>
2020-06-27 02:12:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,7 @@
 katz_deli = []
+other_deli = ["logan", "Avi","Spencer"]
+another_deli = ["Amanda","Annette","Ruchi","Jason","Logan","Spencer", "Avi","Joe","Rachel","Lindsey"]
+
 def line(katz_deli)
   queue_array = []
   if katz_deli.length == 0 
