3dce4fcf12b81c3d02caa7b250ad8588ce98a338
Idalis Paz <idalispaz@gmail.com>
2020-06-27 02:13:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,3 +2,18 @@ katz_deli = []
 other_deli = ["logan", "Avi","Spencer"]
 another_deli = ["Amanda","Annette","Ruchi","Jason","Logan","Spencer", "Avi","Joe","Rachel","Lindsey"]
 
+ expect($stdout).to receive(:puts).with(“The line is currently empty.“)
+        line(katz_deli)
+      end
+    end
+    context “there are people in line” do
+      it “should display the current line” do
+        expect($stdout).to receive(:puts).with(“The line is currently: 1. Logan 2. Avi 3. Spencer”)
+        line(other_deli)
+      end
+      it ‘works with different people in line’ do
+	      expect($stdout).to receive(:puts).with(“The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey”)
+	line(another_deli)
+      end
+    end
+  end
