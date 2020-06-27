1af057152db3aae70c55c278a200a346553e8b08
Idalis Paz <idalispaz@gmail.com>
2020-06-27 01:54:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+katz_deli = []
+def line(katz_deli)
+  queue_array = []
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+ else  katz_deli.each do |costumer|
+      queue_array << ("#{index}. #{name}")
+    end
+    puts "The line is currently: #{queue_array.join(" ")}"
+  end 
+end 
