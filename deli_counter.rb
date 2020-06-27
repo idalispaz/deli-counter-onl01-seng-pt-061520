0f483c66fa2982fffa76a94d264a8e3bdb3d9ccb
Idalis Paz <idalispaz@gmail.com>
2020-06-27 02:14:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,10 @@
 let(:katz_deli) { [] }
   let(:other_deli) { [“Logan”, “Avi”, “Spencer”] }
   let(:another_deli) { [“Amanda”, “Annette”, “Ruchi”, “Jason”, “Logan”, “Spencer”, “Avi”, “Joe”, “Rachel”, “Lindsey”] }
-
+  
+describe “#line” do
+    context “there is nobody in line” do
+      it “should say the line is empty” do
  expect($stdout).to receive(:puts).with(“The line is currently empty.“)
         line(katz_deli)
       end
